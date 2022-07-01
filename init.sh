0ff6e6ba43d9785e4dfe3702a0f6edc6b54d3918
Ruk <info@daco.dev>
2022-07-01 13:10:50 +0900

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir mstdn
+curl https://raw.githubusercontent.com/mastodon/mastodon/main/.env.production.sample > mstdn/.env.production
+curl -O https://raw.githubusercontent.com/5ym/mstdn-traefik-swarm/main/mstdn-compose.yml
+echo 'customize compose file'
+vim mstdn-compose.yml
+echo 'setup env file'
+docker compose -f mstdn-compose.yml run --rm mstdn bundle exec rake mastodon:setup
