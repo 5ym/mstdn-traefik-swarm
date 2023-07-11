582c2bc001cfaaafc326a5986bcdd6ad4fb8d85a
Ruk <info@doany.io>
2023-07-11 02:37:38 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p mstdn/db mstdn/redis mstdn/system
-curl https://raw.githubusercontent.com/mastodon/mastodon/main/.env.production.sample > mstdn/.env.production
+touch mstdn/.env.production
 curl -O https://raw.githubusercontent.com/5ym/mstdn-traefik-swarm/main/mstdn-compose.yml
 echo 'customize compose file'
 sleep 5
@@ -10,3 +10,4 @@ echo 'setup env file'
 docker compose -f mstdn-compose.yml up -d db redis
 docker compose -f mstdn-compose.yml run --rm mstdn bundle exec rake mastodon:setup
 docker compose -f mstdn-compose.yml down
+docker stack -c mstdn-compose.yml deploy mstdn
