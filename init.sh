8944dbc15e4ba6520e0ed29df598ffa1ed06d150
Ruk <info@doany.io>
2023-07-11 02:33:24 -0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,4 +7,6 @@ echo 'customize compose file'
 sleep 5
 vim mstdn-compose.yml
 echo 'setup env file'
+docker compose -f mstdn-compose.yml up -d db redis
 docker compose -f mstdn-compose.yml run --rm mstdn bundle exec rake mastodon:setup
+docker compose -f mstdn-compose.yml down
